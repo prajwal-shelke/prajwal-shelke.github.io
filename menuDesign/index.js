39764dd245489b7951a3430aebd6405ea25b11ec
Prajwalshelke <prajwal@dreamsnama.com>
2023-03-25 19:07:05 +0530

    .

--- menuDesign/index.js
+++ menuDesign/index.js
@@ -1,6 +1,18 @@
 const wrapper = document.getElementById("wrapper");
 
+const configArr = [
+  [1, 1],
+  [1, 2],
+  [1, 3],
+  [2, 1],
+  [2, 2],
+  [2, 3],
+  [3, 1],
+];
+
 setInterval(() => {
-  wrapper.dataset.configuration = Math.floor(Math.random() * 3) + 1;
-  wrapper.dataset.roundness = Math.floor(Math.random() * 3) + 1;
-}, 2000);
+  const randomConfig = configArr[Math.floor(Math.random() * 7)];
+  wrapper.dataset.configuration = randomConfig[0];
+  wrapper.dataset.roundness = randomConfig[1];
+  console.log(wrapper.dataset.configuration, wrapper.dataset.roundness);
+}, 2500);
