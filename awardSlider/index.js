505cbd040044fd19cf1bf707ef3def86056f5730
Prajwalshelke <prajwal@dreamsnama.com>
2023-03-26 13:36:23 +0530

    .

--- awardSlider/index.js
+++ awardSlider/index.js
@@ -34,3 +34,44 @@ window.onmouseup = () => {
   track.dataset.mouseDownAt = "0";
   track.dataset.prevPercentage = track.dataset.percentage;
 };
+
+// for mobile devices
+
+if (window.outerWidth < 800) {
+  window.ontouchstart = (e) => {
+    track.dataset.mouseDownAt = e.touches[0].clientX;
+  };
+
+  window.ontouchmove = (e) => {
+    if (track.dataset.mouseDownAt === "0") return;
+    const mouseDelta =
+      parseFloat(track.dataset.mouseDownAt) - e.touches[0].clientX;
+    const maxDelta = window.innerWidth / 2;
+    const percentage = (mouseDelta / maxDelta) * -100;
+    let newPercentage = percentage + parseFloat(track.dataset.prevPercentage);
+
+    newPercentage = Math.max(Math.min(newPercentage, 0), -100);
+
+    track.dataset.percentage = newPercentage;
+    track.animate(
+      {
+        transform: `translate(${newPercentage}%, -50%)`,
+      },
+      { duration: 1300, fill: "forwards", easing: "linear" }
+    );
+    for (const image of track.getElementsByClassName("image")) {
+      image.animate(
+        {
+          objectPosition: `${newPercentage + 100}% 50%`,
+        },
+        { duration: 1200, fill: "forwards", easing: "linear" }
+      );
+    }
+    console.log(newPercentage + 100);
+  };
+
+  window.ontouchend = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+  };
+}
